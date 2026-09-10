fcb700f452255f0d618aa78305dc7d546a9f4231
Core-vanta <puremundex@gmail.com>
2026-09-10 14:37:26 +0000

    ready for release build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,24 +15,17 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-subprojects {
-    project.evaluationDependsOn(":app")
-}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 
 subprojects {
-        afterEvaluate {
-                    if (extensions.findByName("android") != null) {
-                                    configure<com.android.build.gradle.BaseExtension> {
-                                                        compileSdkVersion(36)
-                                    }
-                    }
+    afterEvaluate {
+        if (extensions.findByName("android") != null) {
+            configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(36)
+            }
         }
+    }
 }
-
-                                    
-                    
-        
