d6a31cf0a77539bd7dd719242c37d5dc9b4bfc94
Core-vanta <puremundex@gmail.com>
2026-09-13 08:28:12 +0000

    fix: ready for build

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,8 +18,8 @@ pluginManagement {
 }
 
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("com.android.library") version "8.6.0" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("com.android.library") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false // <-- ADD THIS
 } 
 
