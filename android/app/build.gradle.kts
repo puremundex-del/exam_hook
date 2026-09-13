2eb0e2c6d2a4588064c2aee5eb557af73ba83d8a
Core-vanta <puremundex@gmail.com>
2026-09-13 11:19:58 +0000

    fix: ready for build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.exam_hook"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
