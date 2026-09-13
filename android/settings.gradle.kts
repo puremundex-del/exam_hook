f7808e9200fb34b64dd0df3e9996fd98df84425b
Core-vanta <puremundex@gmail.com>
2026-09-13 09:19:23 +0000

    chore: update kotlin to 2.2.20 and agp to 8.11.1

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -20,7 +20,7 @@ pluginManagement {
 plugins {
     id("com.android.application") version "8.11.1" apply false
     id("com.android.library") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false // <-- ADD THIS
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false // <-- ADD THIS
 } 
 
 include(":app")
