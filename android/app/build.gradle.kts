2155c4d79a8661472fd583d68f21d7f87bd9ba9c
Core-vanta <puremundex@gmail.com>
2026-09-13 11:44:45 +0000

    fix: add kotlin plugin + jvmTarget 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,17 +1,22 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android") // <-- ADDED THIS
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.exam_hook"
-    compileSdk = 36
+    compileSdk = 36 // <-- Already fixed for file_picker
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions { // <-- Using kotlinOptions works better with AGP 8.11
+        jvmTarget = "17"
     }
 
     defaultConfig {
@@ -21,10 +26,6 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
-        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
-        // flag during build.
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -35,12 +36,6 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
