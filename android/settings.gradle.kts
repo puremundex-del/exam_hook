282c9cf7183f79ee9b05b8ee06f5aad4bc90d43b
Core-vanta <puremundex@gmail.com>
2026-09-13 10:24:20 +0000

    fix: correct settings.gradle.kts Kotlin DSL syntax

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,26 +1,29 @@
 pluginManagement {
-    def flutterSdkPath = {
-        def properties = new Properties()
-        file("local.properties").withInputStream { properties.load(it) }
-        def flutterSdkPath = properties.getProperty("flutter.sdk")
-        assert flutterSdkPath != null, "flutter.sdk not set in local.properties"
-        return flutterSdkPath
-    }()
+        val flutterSdkPath = {
+                    val properties = java.util.Properties()
+                            file("local.properties").inputStream().use { properties.load(it) }
+                                    val flutterSdkPath = properties.getProperty("flutter.sdk")
+                                            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+                                                    flutterSdkPath
+        }()
 
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+            includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+                repositories {
+                            google()
+                                    mavenCentral()
+                                            gradlePluginPortal()
+                }
 }
 
 plugins {
-    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
-    id "com.android.application" version "8.11.1" apply false
-    id "com.android.library" version "8.11.1" apply false
-    id "org.jetbrains.kotlin.android" version "2.2.20" apply false
+        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+            id("com.android.application") version "8.11.1" apply false
+                id("com.android.library") version "8.11.1" apply false
+                    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
+
+                
+        
